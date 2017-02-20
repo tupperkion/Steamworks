5af8c1d9daff49d38d3423d746faca399a44b49e
Tupster24 <tupperkion@gmail.com>
2017-02-20 11:29:04 -0500

    A bunch of stuff. Like, a lot. Probably should've kept this up-to-date.

---
+++ src/org/usfirst/frc5506/Steamworks/Vision.java
@@ -0,0 +1,69 @@
+package org.usfirst.frc5506.Steamworks;
+
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
+public class Vision {
+	public static NetworkTable table;
+	
+	// horizontal FOV of camera
+	public static final double cameraFOV = Math.toRadians(62.2d);
+	public static final int cameraWidth = 480;
+	
+	// distance from middle of bot to camera
+	public static final double offset = 8.5d;
+	// make calculations more convenient
+	public static final double c = offset;
+	
+	public static boolean inited = false;
+	
+	public static void init() {
+		NetworkTable.initialize();
+		table = NetworkTable.getTable("vision");
+		inited = true;
+	}
+	
+	// can the camera see and ID the tape?
+	public static boolean izgud() {
+		return table.getBoolean("izgud", false);
+	}
+	
+	public static double getCameraDistance() {
+		return table.getNumber("distance", 0d);
+	}
+	
+	public static double getError() {
+		return table.getNumber("error", 0d);
+	}
+	
+	public static double getCameraAngle() {
+		return getError() / cameraWidth * cameraFOV + 0.5 * Math.PI;
+	}
+	
+	// distance from gear to robot
+	public static double getDistance() {
+		double a = getCameraDistance();
+		double B = getCameraAngle();
+		return Math.sqrt(Math.pow(a, 2) + Math.pow(c, 2) + 2 * a * c * Math.cos(B));
+	}
+	
+	/**
+	 * Calculates turning angle. Provide "distance" to skip calculations.
+	 * 
+	 * @param distance	the distance from the robot to the tape (getDistance())
+	 */
+	public static double getTurningAngle(double distance) {
+		double a = getCameraDistance();
+		double B = getCameraAngle();
+		double b = distance;
+		if (a <= c)
+			return Math.toDegrees(Math.asin(a * (Math.sin(B) / b)));
+		return 180 - Math.toDegrees(B - Math.asin(c * (Math.sin(B) / b)));
+	}
+	
+	/**
+	 * Calculates turning angle. Calculates absolute distance for convenience.
+	 */
+	public static double getTurningAngle() {
+		return getTurningAngle(getDistance());
+	}
+}
